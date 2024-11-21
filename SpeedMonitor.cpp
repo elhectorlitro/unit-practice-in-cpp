f3149200f4350b11025a28b33bcb7e0ce867f352
elhectorlitro <87398353+elhectorlitro@users.noreply.github.com>
2024-11-21 11:07:28 -0600

    Update SpeedMonitor.cpp

--- SpeedMonitor.cpp
+++ SpeedMonitor.cpp
@@ -3,16 +3,6 @@
 #include <string>
 using namespace std;
 
-class BNFSpeedSensor{
-        public:
-            int   getCurrentSpeed(){
-                std::random_device rd;
-                std::mt19937 gen(rd());
-                std::uniform_int_distribution<> distr(1, 100);
-                return distr(gen);
-            }
-};
-
 class SpeedMonitor{
     private : int _speedThreshold;
   
